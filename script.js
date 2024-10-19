29cedb8ea78c931fe9d5d1bfce2f5c695c953871
Aakash Prajapati <aakashap309@gmail.com>
2024-10-19 19:43:13 +0530

    asd

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     if (localStorage.getItem('theme') === 'light') {
         $('body').addClass('light-theme');
         $('#theme-toggle i').removeClass('bi-sun-fill').addClass('bi-moon-fill'); // Set to moon icon
-        $('#theme-toggle').attr('title', 'Switch to Dark Theme'); // Set title for light theme
+        $('#theme-toggle').attr('title', 'Switch to Light Theme'); // Set title for light theme
     } else {
-        $('#theme-toggle').attr('title', 'Switch to Light Theme'); // Set title for dark theme
+        $('#theme-toggle').attr('title', 'Switch to Dark Theme'); // Set title for dark theme
     }
 
     $('#theme-toggle').on('click', function() {
@@ -16,11 +16,11 @@ $(document).ready(function() {
         if ($('body').hasClass('light-theme')) {
             icon.removeClass('bi-sun-fill').addClass('bi-moon-fill'); 
             localStorage.setItem('theme', 'light'); // Store light theme preference
-            $(this).attr('title', 'Switch to Dark Theme'); // Update title
+            $(this).attr('title', 'Switch to Light Theme'); // Update title
         } else {
             icon.removeClass('bi-moon-fill').addClass('bi-sun-fill'); 
             localStorage.setItem('theme', 'dark'); // Store dark theme preference
-            $(this).attr('title', 'Switch to Light Theme'); // Update title
+            $(this).attr('title', 'Switch to dark Theme'); // Update title
         }
     });
 });
